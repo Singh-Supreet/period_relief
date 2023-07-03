6e2990ad1267627adf29c2114fd68fb75d0d12e2
Supreet Singh <singhsupreet221@gmail.com>
2023-07-03 21:04:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// let image = document.getElementById('image');
+// let images = ['image1.jpg', 'image3.jpg', 'image4.jpg']
+// setInterval(function () {
+//     let random = Math.floor(Math.random() * 3);
+//     image.src = images[random];
+// }, 800)
